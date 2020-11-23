c66e51aeb3bdc83fe5fb484163447f102698ea73
shuyu <shq219@lehigh.edu>
2020-11-23 11:58:52 -0500

    11.23.3

---
+++ faster_code/codes/util/core.py
@@ -0,0 +1,48 @@
+"""
+
+"""
+
+import tensorflow as tf
+import numpy as np
+
+def non_linear_fn(t, x, y, z):
+  # returns a function from variables
+  return tf.nn.tanh(20*(t - 2*(x-.5)))+ tf.nn.selu((t-2*(y-0.5))) + tf.nn.sigmoid(-20*(t-(z-0.5)))
+
+
+#"""[Summary]
+
+#:param [ParamName]: [ParamDescription], defaults to [DefaultParamVal]
+#:type [ParamName]: [ParamType](, optional)
+#...
+#:raises [ErrorType]: [ErrorDescription]
+# ...
+# :return: [ReturnDescription]
+# :rtype: [ReturnType]
+# """
+
+def generate_data(values, function=non_linear_fn, length=25, range_=[-1, 1]):
+    """
+    Function to generate data from values
+
+    :param values: values to function for generating spectra
+    :type values: float
+    :param function:  mathematical expression used to generate spectra
+    :type function: function, optional
+    :param length: spectral length
+    :type length: int (optional)
+    :param range_: x range for function
+    :type range_:  list of float
+    :return: generatered spectra
+    :rtype: array of float
+    """
+
+
+    x = np.linspace(range_[0], range_[1], 25)
+
+    data = np.zeros((values.shape[0], 25))
+
+    for i in range(values.shape[0]):
+        data[i, :] = function(x, values[i, 0], values[i, 1], values[i, 2])
+
+    return data
---
+++ faster_code/source/index.rst
@@ -0,0 +1,20 @@
+.. Better than Better, Less Bias than Less Bias Machine Learning: Electromechanical Switching in Ferroelectric Thin Films documentation master file, created by
+   sphinx-quickstart on Mon Nov 23 11:25:13 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Better than Better, Less Bias than Less Bias Machine Learning: Electromechanical Switching in Ferroelectric Thin Films's documentation!
+==================================================================================================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
