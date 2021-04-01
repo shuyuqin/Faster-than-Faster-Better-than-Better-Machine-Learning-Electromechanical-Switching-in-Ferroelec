8016b4cea545aea28f9ffbc32f62463dad56a3c8
shuyu <shq219@lehigh.edu>
2021-04-01 16:01:48 -0400

    4.1.6

--- faster_code/codes/util/core.py
+++ faster_code/codes/util/core.py
@@ -494,10 +494,10 @@ def embedding_maps_movie(data, image, printing, folder, beta,loss,
 
 
     fig.tight_layout(pad=1)
-    fig.set_size_inches(13, 13)
+ #   fig.set_size_inches(13, 13)
     # saves the figure
-    fig.savefig(folder + '/' + filename +'.png', dpi=300)
-    #savefig(folder + '/' + filename, printing)
+ #   fig.savefig(folder + '/' + filename +'.png', dpi=300)
+    savefig(folder + '/' + filename, printing)
 
     return(fig)
 
