1c1e37178f87e6dc89d83a5425fc77cb13156d6e
shuyu <shq219@lehigh.edu>
2021-04-02 01:09:51 -0400

    4.2.0

--- faster_code/codes/util/core.py
+++ faster_code/codes/util/core.py
@@ -570,7 +570,7 @@ def training_images(model,
 
         # plots the embedding maps
         _ = embedding_maps_movie(embedding_exported[name_extraction(file_list)], image,printing_,
-                           folder, beta, loss_, filename='./' + file_name + '_epoch_{0:04}'.format(i))
+                           folder, beta, loss_, filename='./' + file_name + '_epoch_{0:04}'.format(i+1007))
 
         # Closes the figure
         plt.close(_)
